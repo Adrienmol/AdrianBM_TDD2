060158ee12d41da15116a03825a2300e6f9970cd
Adrienmol <adrianballester95@gmail.com>
2023-03-07 18:14:33 +0100

    Tercer Commit (Ejercicio 12 FizzBuzz)

--- Tareas-TDD-ABM/Tests/FizzBuzzTest.java
+++ Tareas-TDD-ABM/Tests/FizzBuzzTest.java
@@ -51,4 +51,37 @@ class FizzBuzzTest {
         assertEquals("Fizz", FizzBuzz);
         System.out.println("Resultado:" + " " + FizzBuzz );
     }
+
+    @Test
+    void testAlPasarle7Devuelve7() {
+        FizzBuzz prueba = new FizzBuzz();
+        String FizzBuzz = prueba.imprimirResultado(7);
+        assertEquals("7", FizzBuzz);
+        System.out.println("Resultado:" + " " + FizzBuzz );
+    }
+
+    @Test
+    void testAlPasarle8Devuelve8() {
+        FizzBuzz prueba = new FizzBuzz();
+        String FizzBuzz = prueba.imprimirResultado(8);
+        assertEquals("8", FizzBuzz);
+        System.out.println("Resultado:" + " " + FizzBuzz );
+    }
+
+    @Test
+    void testAlPasarle9DevuelveFizz() {
+        FizzBuzz prueba = new FizzBuzz();
+        String FizzBuzz = prueba.imprimirResultado(9);
+        assertEquals("Fizz", FizzBuzz);
+        System.out.println("Resultado:" + " " + FizzBuzz );
+    }
+
+    @Test
+    void testAlPasarle10DevuelveBuzz() {
+        FizzBuzz prueba = new FizzBuzz();
+        String FizzBuzz = prueba.imprimirResultado(10);
+        assertEquals("Buzz", FizzBuzz);
+        System.out.println("Resultado:" + " " + FizzBuzz );
+    }
+
 }
